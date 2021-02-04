f7926580f9fbfe0cf45884a3d6240eb8ce3c687d
cp <cp@DESKTOP-PSC1POC>
2021-02-04 12:16:17 +0300

    First Commit

---
+++ FirstPracticalHomework/src/FirstPracticalHomework.java
@@ -0,0 +1,13 @@
+//This program is written by Nawaf Alwadaei
+//Date: Feb 4,2021
+//Course Name: 212CSS-4 Object Oriented Programming
+
+public class FirstPracticalHomework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		System.out.println("Nawaf Alwadaei");
+	}
+
+}
